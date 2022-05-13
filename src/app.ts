a1482638eae8be3a9ddb70c84c5b4dad8e392eca
dcmatheus <mathws.costag@gmail.com>
2022-05-13 01:13:25 -0300

    create order routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import orders from './routes/orders';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/orders', orders);
 
 export default app;
